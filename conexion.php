3324568be70c5ec9fe181169dd45ab867458ef33
Manuel Cruz <cruzmanuelar@gmail.com>
2020-09-03 17:18:01 -0500

    archivos eliminados

--- conexion.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	function conexion()
-	{
-		return $conexion=mysqli_connect("mysql-cruzmanuelar.alwaysdata.net","212641","manu132012","cruzmanuelar_bbdd");
-	}
- ?>
--- dbdbd.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conex = mysqli_connect("mysql-cruzmanuelar.alwaysdata.net","212641","manu132012","cruzmanuelar_bbdd"); 
-
-?>
